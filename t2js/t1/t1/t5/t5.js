c27a1ec27e3dfc26bc586e7189299b4eca487a4e
veeti174 <veeti.isokangas@metropolia.fi>
2025-08-25 00:01:20 +0300

    update files

--- t2js/t1/t1/t5/t5.js
+++ t2js/t1/t1/t5/t5.js
@@ -1,16 +1,23 @@
 'use strict';
 
-const The_intgr = +prompt('Gimme a positive integer ');
-const base = +prompt('Enter starting number')
+const array = [8, 23, 105, 42, 7, 66, 19, 90];
 
-let sum = 0;
-let numbers = [];
+let order = prompt("Type 'asc' for ascending or 'desc' for descending order:").toLowerCase();
 
-for (let i = base; i <= The_intgr; i++) {
-  sum += i;
-  numbers.push(i);
-  console.log(i);
+function sortArray(arr, order) {
+  if(order === 'asc') {
+    return [...arr].sort((a, b) => a -b);
+  } else if (order === 'desc') {
+    return [...arr].sort((a, b) => b - a);
+  } else {
+    console.error("Invalid order, Use 'asc' or 'desc'.");
+    return arr;
+  }
 }
 
-document.querySelector('#Target').innerHTML =
- `Natural numbers sum is tai se summa  ${sum}`;
+const sortedArray = sortArray(array, order);
+console.log('Original array:', array);
+console.log('Sorted array:', sortedArray);
+
+document.getElementById('target').innerText =
+  `Original array: ${array}\nSorted array: ${sortedArray}`;
